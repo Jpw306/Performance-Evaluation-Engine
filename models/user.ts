68961fa172f58f68b96f735dc2d4e072174e682e
RyanGarfinkel <113050972+RyanGarfinkel@users.noreply.github.com>
2025-10-25 17:19:01 -0400

    use user and profile

--- models/user.ts
+++ models/user.ts
@@ -1,9 +1,10 @@
 interface User {
-  id: string,             // global user id
-  name: string;           // user's real name
-  photoIcon: string;      // link to user's github icon
-  githubId: string;       // user's github username 
-  clashRoyaleTag: string; // user's clash royal tag (#CRTAG)
+  id: string;
+  name: string;
+  photoIcon: string;
+  githubUsername: string;
+  clashRoyaleTag: string;
 }
 
 export type { User };
+
