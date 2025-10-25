ccde3191dd8c9c01f176237a3dc007c4deab8fd8
RyanGarfinkel <113050972+RyanGarfinkel@users.noreply.github.com>
2025-10-25 18:25:02 -0400

    lint fix

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -2,10 +2,10 @@ import Link from 'next/link';
 
 export default function NavBar() {
   return (
-    <nav className="flex justify-center gap-6 text-neutral-300">
-      <Link href="/" className="hover:text-yellow-400">Compass</Link>
-      <Link href="/leaderboard" className="hover:text-yellow-400">Leaderboard</Link>
-      <Link href="/dashboard" className="hover:text-yellow-400">Dashboard</Link>
+    <nav className='flex justify-center gap-6 text-neutral-300'>
+      <Link href='/' className='hover:text-yellow-400'>Compass</Link>
+      <Link href='/leaderboard' className='hover:text-yellow-400'>Leaderboard</Link>
+      <Link href='/dashboard' className='hover:text-yellow-400'>Dashboard</Link>
     </nav>
   );
 }
