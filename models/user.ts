6076b1c097d2d486f798e35ec8a25526c164e8f4
PlasticRosewood <jdtomjones@gmail.com>
2025-10-25 16:10:54 -0400

    Add user id field to frontend user schema

--- models/user.ts
+++ models/user.ts
@@ -1,8 +1,9 @@
 interface User {
-  name: string;
-  photoIcon: string;
-  githubId: string;
-  clashRoyaleTag: string;
+  id: string,             // global user id
+  name: string;           // user's real name
+  photoIcon: string;      // link to user's github icon
+  githubId: string;       // user's github username 
+  clashRoyaleTag: string; // user's clash royal tag (#CRTAG)
 }
 
 export type { User };
