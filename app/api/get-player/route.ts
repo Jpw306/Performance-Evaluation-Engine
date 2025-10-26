554a5844ba965724cd3563401b636735c15b107c
RyanGarfinkel <113050972+RyanGarfinkel@users.noreply.github.com>
2025-10-26 06:02:39 -0400

    1

--- app/api/get-player/route.ts
+++ app/api/get-player/route.ts
@@ -1,4 +1,3 @@
-/* === GET ALL INFO ASSOCIATED WITH A USER'S CLASH ROYALE ACCOUNT === */
 
 import { NextResponse } from 'next/server';
 import { CLASH_API_BASE_URL } from '@/lib/constants';
@@ -7,16 +6,13 @@ export async function GET(request : Request) {
     const { searchParams } = new URL(request.url);
     const userId = searchParams.get('userId');
 
-    if (!userId) {
+    if (!userId)
         return NextResponse.json({ error: 'Missing userId parameter' }, { status: 400 });
-    }
 
-    // userId is actually the clashRoyaleTag, use it directly
     const clashId = userId;
         
     if (!clashId) return NextResponse.json({error: 'Missing ClashID Tag!'}, {status: 400});
 
-    // forward to clash api 
     const res = await fetch(`${CLASH_API_BASE_URL}/players/${encodeURIComponent(clashId)}`, {
         headers: { Authorization: `Bearer ${process.env.CLASH_API_TOKEN}`}
     });
