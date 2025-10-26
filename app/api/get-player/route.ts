e3846cb6c127b2bb569b3761ec7969ec1554c2ef
RyanGarfinkel <113050972+RyanGarfinkel@users.noreply.github.com>
2025-10-26 06:03:51 -0400

    session fix

--- app/api/get-player/route.ts
+++ app/api/get-player/route.ts
@@ -1,15 +1,51 @@
 
 import { NextResponse } from 'next/server';
 import { CLASH_API_BASE_URL } from '@/lib/constants';
+import dbConnect from '@/lib/mongodb';
+import { User } from '@/models/backend/user';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '../auth/[...nextauth]/route';
+
+interface SessionUser {
+  githubUsername?: string;
+  name?: string;
+  email?: string;
+  image?: string;
+};
 
 export async function GET(request : Request) {
-    const { searchParams } = new URL(request.url);
-    const userId = searchParams.get('userId');
 
-    if (!userId)
-        return NextResponse.json({ error: 'Missing userId parameter' }, { status: 400 });
+    const session = await getServerSession(authOptions);
 
-    const clashId = userId;
+    if (!session || !session.user)
+          return NextResponse.json({ error: 'Not authenticated' }, { status: 401 });
+    
+    const githubUsername = (session.user as SessionUser).githubUsername;
+
+    if (!githubUsername)
+          return NextResponse.json({ error: 'GitHub username not found in session' }, { status: 400 });
+
+    let clashId;
+    try {
+        await dbConnect();
+
+        const user = await User.findOne({ githubUsername: githubUsername });
+
+        if (!user) {
+            return NextResponse.json(
+                { error: 'User not found' },
+                { status: 404 }
+            );
+        }
+
+        clashId = user.clashRoyaleTag;
+    } catch (error) {
+        console.error('Err fetching user:', error);
+        return NextResponse.json(
+                { error: 'Internal server error' },
+                { status: 500 }
+        );
+    }
         
     if (!clashId) return NextResponse.json({error: 'Missing ClashID Tag!'}, {status: 400});
 
