c50228f262ef828e34e4eff0f096bef9be1dc04a
n8banks <nathan@akhalls.com>
2025-10-26 07:05:07 -0400

    added group LB

--- app/leaderboard/page.tsx
+++ app/leaderboard/page.tsx
@@ -1,3 +1,4 @@
+import GroupLeaderboard from '@/components/GroupLeaderboard';
 import LeaderboardTable from '@/components/LeaderboardTable';
 import NavBar from '@/components/NavBar';
 
@@ -7,7 +8,7 @@ export default function LeaderboardPage() {
       <NavBar />
       <div className="max-w-5xl mx-auto mt-10">
         <h1 className="text-3xl font-headline mb-6 text-clash-gold drop-shadow">Leaderboard</h1>
-        {/* <LeaderboardTable /> */}
+        <GroupLeaderboard/>
       </div>
     </main>
   );
